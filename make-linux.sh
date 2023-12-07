a6e3129700995ad1c8edc6eeb9d21d7a3a6edfbb
Piecuch Pawel <piecuch.pawel@gmail.com>
2023-12-07 14:12:24 +0100

    cleanup build issues

--- make-linux.sh
+++ make-linux.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [ -x /proc/cpuinfo ]; then
-  grep -q BCM2708 /proc/cpuinfo
+set -e
+
+if [ -e /proc/cpuinfo ]; then
+  egrep -q 'BCM2708|BCM2711' /proc/cpuinfo
   if [ $? = 0 ]; then
+    echo "=== ENABLE RASPBERRY PI ==="
     extra_flags="-DRASPBERRY_PI $extra_flags"
   fi
 fi
 
-make -f Makefile.linux dist USER="$extra_flags"
+cmd=${1:-dist}
+
+make -f Makefile.linux $cmd USER="$extra_flags"
